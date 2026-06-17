d8463ee96820a0d3c2627249063568c6f32b92d8
dolatjangir <dolatjangir778@gmail.com>
2026-06-17 12:26:41 +0530

    add:pwa

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,31 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+//    images: {
+//       domains: ['res.cloudinary.com'],
+//     unoptimized: true,
+//   },
+// };
+
+// export default nextConfig;
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+   register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = {
   reactCompiler: true,
-   images: {
-      domains: ['res.cloudinary.com'],
+
+  images: {
+    domains: ["res.cloudinary.com"],
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
